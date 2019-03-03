16247f2028503eb4516de56a18b239aa3894436a
unknown <Student21>
2019-03-03 11:20:26 +0300

    initial commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/user/{id}", name="user", requirements={"id"="\d+"})
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+
+        $user = $repository->find($id);
+
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+            'posts' => $user->getPosts()
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile() {
+
+        $user = $this->getUser();
+
+        $postsRepository = $this->getDoctrine()->getRepository(Post::class);
+
+        $posts = $postsRepository->findByUser($user);
+
+        return $this->render('user/profile.html.twig', [
+            'posts' => $posts,
+            'user' => $user,
+        ]);
+    }
+}
